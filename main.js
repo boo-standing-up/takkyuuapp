d9f81511fe5bf9bda33727896a094009f8bc6fa8
boo-standing-up <70996914+boo-standing-up@users.noreply.github.com>
2023-02-25 11:31:40 +0000

    🎨

---
+++ main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let baka2, baka;
+let n;
+
+function addCount() {
+  n++;
+  counter.innerHTML = n;
+}
+
+window.addEventListener("click", () => {
+  counter = document.getElementById("baka2");
+  btnAdd = document.getElementById("baka");
+  n = 0;
+
+  btnAdd.addEventListener("click", addCount);
+});
